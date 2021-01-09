4a0f704ae4d1b123e283882f487dc3c9413e879d
Timothy Jones <t.jones@aptacode.com>
2021-01-09 14:43:08 +0000

    Added shapes to CanvasDemo

--- BlazorCanvas.Demo/Pages/Index.razor.cs
+++ BlazorCanvas.Demo/Pages/Index.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
@@ -9,7 +10,7 @@ namespace Aptacode.BlazorCanvas.Demo.Pages
         #region Properties
         public ElementReference Canvas { get; set; }
 
-        [Inject] public BlazorCanvasInterop BlazorCanvasInterop { get; set; }
+        [Inject] public BlazorCanvasInterop ctx { get; set; }
 
 
         #endregion
@@ -20,13 +21,42 @@ namespace Aptacode.BlazorCanvas.Demo.Pages
         {
             if (firstRender)
             {
-                await BlazorCanvasInterop.Register(Canvas);
+                await ctx.Register(Canvas);
             }
 
-            BlazorCanvasInterop.StrokeStyle("green");
-            BlazorCanvasInterop.Ellipse(30, 30, 30, 30, (float)Math.PI, 0, 2 * (float)Math.PI);
-            BlazorCanvasInterop.Stroke();
-            
+            //Ellipse
+            ctx.LineWidth(2);
+            ctx.StrokeStyle("green");
+            ctx.FillStyle("white");
+            ctx.Ellipse(40, 40, 30, 30, (float)Math.PI, 0, 2 * (float)Math.PI);
+            ctx.Stroke();
+            ctx.Fill();
+
+            //Text
+            ctx.TextAlign("center");
+            ctx.FillStyle("black");
+            ctx.FillText("Text", 150, 30);
+
+            //Path
+            ctx.StrokeStyle("black");
+            ctx.LineWidth(4);
+            ctx.BeginPath();
+            ctx.MoveTo(100,50);
+            ctx.LineTo(75, 80);
+            ctx.LineTo(90, 130);
+            ctx.Stroke();
+
+            //Polygon
+            ctx.FillStyle("gray");
+            ctx.BeginPath();
+            ctx.MoveTo(30, 80);
+            ctx.LineTo(30, 150);
+            ctx.LineTo(60, 150);
+            ctx.ClosePath();
+            ctx.Stroke();
+            ctx.Fill();
+
+
             await base.OnAfterRenderAsync(firstRender);
         }
 
